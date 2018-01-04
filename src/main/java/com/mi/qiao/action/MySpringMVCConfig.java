fe7a034becac1b66cbe8fb6fc25926d29539790e
qiaocy <18568437879@163.com>
2018-01-04 18:13:49 +0800

    CSS 引入

--- src/main/java/com/mi/qiao/action/MySpringMVCConfig.java
+++ src/main/java/com/mi/qiao/action/MySpringMVCConfig.java
@@ -1,17 +1,39 @@
 package com.mi.qiao.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mi.qiao.Service.UserService;
+import com.mi.qiao.entity.User;
 
 @Controller
 @RequestMapping("/action")
 public class MySpringMVCConfig {
 
+	@Autowired
+	private UserService userServic;
+	
 	@RequestMapping("/register")
 	public String register() {
 		return "login/register";
 	}
 	
+	@RequestMapping("/login")
+	public String login() {
+		return "login/login";
+	}
 	
+	@RequestMapping("/userlist")
+	public String userList(Model model) {
+		List<User> list = new ArrayList<>();
+		list = userServic.selectAll();
+		model.addAttribute("userlist", list);
+		return "userlist";
+	}
 }
